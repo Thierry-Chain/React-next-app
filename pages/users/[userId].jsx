671b009d50e35d1350278b1f7a5f0d2ffb88eee6
Thierry-Chain <irambonat0@gmail.com>
2021-09-05 19:17:26 +0200

    finished using `getStaticPaths` with `fallback` property set to --` true`/false--

--- pages/users/[userId].jsx
+++ pages/users/[userId].jsx
@@ -1,26 +1,39 @@
 import axios from 'axios'
-import { Box, Button, Text, Link, Flex } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { Box, Button, Text, Link, Flex, Skeleton } from '@chakra-ui/react'
 export const getStaticPaths = async () => {
   const res = await axios('http://localhost:3004/posts/')
-  const paths = res.data.map((user) => {
+  const paths1 = res.data.map((user) => {
     return {
       params: { userId: user.id.toString() },
     }
   })
-  return { paths, fallback: false }
+  const paths = [paths1[0], paths1[1], paths1[2]]
+  return { paths, fallback: true }
 }
 export const getStaticProps = async (context) => {
   const userId = context.params.userId
   const res = await axios(`http://localhost:3004/posts/${userId}`)
   const data = res.data
-  //console.log('hey bosss', userId)
+  console.log(`building /users/${userId}`)
+  if (!data.id) {
+    console.log('invalid route')
+    return {
+      notFound: true,
+    }
+  }
   return {
     props: { user: data },
   }
 }
 
 export default function User({ user }) {
-  console.log(user)
+  //console.log(user)
+  const router = useRouter()
+  if (router.isFallback) {
+    console.log('Loading')
+    return <Skeleton height="100px" width="80%" />
+  }
   return (
     <>
       {' '}
