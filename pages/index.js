c38c31d2e6f1e6a409727b77b6885cb015765f29
Thierry-Chain <irambonat0@gmail.com>
2021-09-17 10:58:03 +0200

    test point

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-
-
-
 //import styles from '../styles/Home.module.css'
 import dynamic from 'next/dynamic'
 import { Button, Box, SimpleGrid, Heading, Flex } from '@chakra-ui/react'
