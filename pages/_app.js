227e9e6add199b736d976d7767b60b9a5810b8d9
Thierry-Chain <irambonat0@gmail.com>
2021-09-18 16:47:41 +0200

    added add new file feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+export const queryClient = new QueryClient(/*{
+  defaultOptions: {
+    queries: { refetchOnWindowFocus: true },
+  },
+}*/)
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Layout>
+
+      </ChakraProvider></QueryClientProvider>
   )
+
 }
 
 export default MyApp
