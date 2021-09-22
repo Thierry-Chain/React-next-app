263f266ebaea72a780ebeca8e39f34e9a73a7074
Thierry-Chain <irambonat0@gmail.com>
2021-09-22 20:32:38 +0200

    added search bar in home page

--- pages/users/all.jsx
+++ pages/users/all.jsx
@@ -5,12 +5,15 @@ import {
   Text,
   Button,
   Skeleton,
+  Input,
+  Flex,
 } from '@chakra-ui/react'
 import axios from 'axios'
 import { queryClient } from '../_app'
 import Head from 'next/head'
 import Link from 'next/link'
 import { useQuery } from 'react-query'
+import { useState } from 'react'
 export const getStaticProps = async () => {
   const res = await axios('http://localhost:3004/posts/')
   const data = res.data
@@ -29,32 +32,58 @@ export const fetcher = async () => {
   return data
 }
 export default function Users({ init }) {
-  console.log(init.slice(0, 5))
+  const [searchKey, setSearchKey] = useState('')
   const { data } = useQuery('getAllUsers', fetcher, {
     initialData: init.slice(0, 5),
+    onSuccess: (dt) => {
+      console.log('data set', data)
+      setAllUsers(dt)
+    },
+  })
+  const [users, setAllUsers] = useState(data)
+
+  const filtered = [...users].filter((data) => {
+    return (
+      data.title.toLowerCase().indexOf(searchKey.toLowerCase().trim()) !== -1 ||
+      data.author.toLowerCase().indexOf(searchKey.toLowerCase().trim()) !== -1
+    )
   })
 
+  const currentUser = searchKey ? filtered : users
   return (
     <>
-      {data?.length ? (
+      <Heading size="md" textAlign="center">
+        Users list
+      </Heading>
+      <Flex>
+        <Input
+          type="text"
+          mx="auto"
+          bg="gray.100"
+          py="3"
+          my="5"
+          placeholder="Search by name "
+          w={['90%', '70%', '65%']}
+          value={searchKey}
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+      </Flex>
+      {currentUser?.length ? (
         <>
           <Head>
-            <title>All users || {data.length}</title>
+            <title>All users || {currentUser.length}</title>
             <link rel="icon" href="/logo.jpg" />
           </Head>
           <Box>
-            <Heading size="md" textAlign="center">
-              Users list
-            </Heading>
             <SimpleGrid columns={[1, 2, 3, 4]} gap="5">
-              {data.map((item) => {
+              {currentUser.map((item) => {
                 return (
                   <Box
                     bg="blackAlpha.100"
                     shadow="lg"
                     rounded="lg"
                     p="4"
-                    key={data.indexOf(item)}
+                    key={currentUser.indexOf(item)}
                     _hover={{ shadow: '2xl', bg: 'gray.300' }}
                   >
                     <Text>Title: {item.title}</Text>
@@ -79,7 +108,7 @@ export default function Users({ init }) {
           </Box>
         </>
       ) : (
-        <Skeleton w="90%" mx="auto" p="5" />
+        <p>No data available {/*<Skeleton w="90%" mx="auto" p="5" /> */}</p>
       )}
     </>
   )
